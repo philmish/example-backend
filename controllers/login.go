952527f78fa2cba2bae37b268746001d99eef388
philmish <thantalosdream@protonmail.com>
2022-07-27 23:56:40 +0200

    feat: add .env file support

--- controllers/login.go
+++ controllers/login.go
@@ -2,10 +2,12 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
-	//"github.com/philmish/example-backend/middleware"
+	"github.com/philmish/example-backend/middleware"
 	"github.com/philmish/example-backend/models"
 )
 
@@ -19,7 +21,23 @@ type LoginResponse struct {
     Is_admin bool `json:"isAdmin"`
 }
 
+func getEnvVars() ([string]string, error) {
+    res := [string]string{}
+    res["key"] = os.Getenv("SECRET")
+    res["db"] = os.Getenv("DB_NAME")
+    for k, v := range res {
+        if v == "" {
+            return res, fmt.Errorf("No value for %s found", k)
+        }
+    }
+    return res, nil
+}
+
 func Login(c *gin.Context) {
+    envVars, err := getEnvVars()
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+    }
     var req LoginRequest
     var user models.User
 
--- main.go
+++ main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
-
+	"os"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/philmish/example-backend/controllers"
 	"github.com/philmish/example-backend/models"
 )
 
 func main() {
-    if err := models.InitDB("./test.db"); err != nil {
+
+    if err := godotenv.Load(".env"); err != nil {
+        log.Fatalf(err.Error())
+    }
+
+    dbName := os.Getenv("DB_NAME")
+
+    if dbName == "" {
+        log.Fatalf("Missing Database name env var")
+    }
+
+    if err := models.InitDB(dbName); err != nil {
         log.Fatalf(err.Error())
     }
 
